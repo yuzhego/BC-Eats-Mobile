cc7f2cb2734cd06fc6e6c7d6cec8a000773729d2
Marcellahuang <57575094+Marcellahuang@users.noreply.github.com>
2020-08-24 02:35:21 -0700

    pages
    Pages for the icons on the navigation bar. The Feed is technically the homepage, but I made a separate homepage file so there would be a Feed file that could be accessed when pressing the icons on the navigation bar.

---
+++ pages/Notifications.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Notifications extends StatefulWidget {
+  Notifications({Key key}) : super(key: key);
+
+  @override
+  _NotificationsState createState() => _NotificationsState();
+}
+
+class _NotificationsState extends State<Notifications> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notifications"),
+      ),
+    );
+  }
+}
